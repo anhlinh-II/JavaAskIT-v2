887a3d636113a35ac45e9ae22c743bf84bc92e51
Anh Linh <linhnguyenanh75@gmail.com>
2024-08-22 23:35:08 +0700

    API Login

--- src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
@@ -1,5 +1,6 @@
 package vn.hoidanit.jobhunter.controller;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 import vn.hoidanit.jobhunter.domain.dto.LoginDTO;
 import vn.hoidanit.jobhunter.domain.dto.ResLoginDTO;
+import vn.hoidanit.jobhunter.service.UserService;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,13 +24,16 @@ public class AuthController {
 
      private final AuthenticationManagerBuilder authenticationManagerBuilder;
      private final SecurityUtil securityUtil;
+     private final UserService userService;
 
      public AuthController(
           AuthenticationManagerBuilder authenticationManagerBuilder,
-          SecurityUtil securityUtil     
+          SecurityUtil securityUtil,
+          UserService userService   
      ) {
           this.authenticationManagerBuilder = authenticationManagerBuilder;
           this.securityUtil = securityUtil;
+          this.userService = userService;
      }
 
      @PostMapping("login")
@@ -38,14 +43,20 @@ public class AuthController {
                new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword());  
           //xác thực người dùng => cần viết hàm loadUserByUsername
           Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-          //nạp thông tin (nếu xử lý thành công) vào SecurityContext
-          // comment because this line of code doesn't exist in the video
-
+          
           // create a token
           String access_token = this.securityUtil.createToken(authentication);
+          //nạp thông tin (nếu xử lý thành công) vào SecurityContext
           SecurityContextHolder.getContext().setAuthentication(authentication);
           
           ResLoginDTO res = new ResLoginDTO();
+
+          vn.hoidanit.jobhunter.domain.User currentUserDB = this.userService.handleGetUserByUsername(loginDTO.getUsername());
+          if (currentUserDB != null) {
+               ResLoginDTO.UserLogin userLogin = new ResLoginDTO.UserLogin(currentUserDB.getId(), currentUserDB.getEmail(), currentUserDB.getName());
+               res.setUser(userLogin);
+          }
+
           res.setAccess_token(access_token);
           return ResponseEntity.ok().body(res);
           
--- src/main/java/vn/hoidanit/jobhunter/domain/dto/ResLoginDTO.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/dto/ResLoginDTO.java
@@ -1,14 +1,23 @@
 package vn.hoidanit.jobhunter.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ResLoginDTO {
      private String access_token;
-
-     public String getAccessToken() {
-          return access_token;
-     }
-
-     public void setAccess_token(String access_token) {
-          this.access_token = access_token;
-     }
+     private UserLogin user;
      
+     @Getter
+     @Setter
+     @AllArgsConstructor
+     @NoArgsConstructor
+     public static class UserLogin {
+          private long id;
+          private String email;
+          private String name;
+     }
 }
